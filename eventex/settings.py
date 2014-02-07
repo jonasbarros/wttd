2c7cf65e83547683b4ddc92f16321b0fc0977c61
Jonas Barros <contato@jonasbarros.com.br>
2014-02-07 00:03:48 -0200

    Projeto Corrigido

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #import os
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-import jd_database_url
+import dj_database_url
 from unipath import Path
 BASE_DIR = Path(__file__).parent
 
