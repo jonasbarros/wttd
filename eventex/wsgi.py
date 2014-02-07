29276c56c8267990a2c948f0755f7b475d5d751f
Jonas Barros <contato@jonasbarros.com.br>
2014-02-07 13:00:08 -0200

    correcao do arquivo wsgi.py

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -7,11 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-#import os
+import os
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
 from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
 
 from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
+
 application = Cling(get_wsgi_application())
